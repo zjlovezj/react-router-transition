56073bd4b6c9349209dabcd5359f6beb9ea67fb7
Richard Maisano <rmaisano@squarespace.com>
2016-04-08 19:30:18 -0400

    demos build for gh-pages.

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var path = require('path');
+
+var src = path.join(__dirname, 'src');
+
+module.exports = {
+  devtool: 'sourcemap',
+  entry: path.join(src, 'index.js'),
+  output: {
+    path: path.join(__dirname, 'lib'),
+    filename: 'react-router-transition.js',
+    sourceMapFilename: 'react-router-transition.map',
+    library: 'ReactRouterTransition',
+    libraryTarget: 'umd'
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      loader: 'babel',
+      include: src
+    }]
+  },
+  resolve: {
+    root: src,
+    extensions: ['', '.js', '.jsx']
+  },
+  eslint: {
+    configFile: '.eslintrc'
+  }
+};
